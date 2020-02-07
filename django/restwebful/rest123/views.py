f1de6f5d12b3c4d19b570926542c86d235f5a5a2
nikosulkava <niko.sulkava@myy.haaga-helia.fi>
2020-02-07 14:31:28 +0200

    working post function

--- django/restwebful/rest123/views.py
+++ django/restwebful/rest123/views.py
@@ -2,7 +2,13 @@ from .models import Drink
 from rest_framework import viewsets
 from .serilalizer import DrinkSerializer
 from django.shortcuts import render
+import os
+import slack
+from dotenv import load_dotenv
+from django.contrib import messages
 
+load_dotenv()
+client = slack.WebClient(token=os.getenv("SLACK_TOKEN"))
 
 # Create your views here.
 
@@ -11,6 +17,14 @@ def home(request):
         'drinks': Drink.objects.all()
     }
     return render(request, 'rest/index.html', context)
+
+def outofbeer(request):
+    response = client.chat_postMessage(
+        channel='#general',
+        text="Saunatilasta on juomat loppu. Äkkiä lisää!")
+
+    messages.success(request, 'Viesti lähetetty slack-kanavalle! Lisää olutta tulossa!')
+    return render(request, 'rest/index.html')  
 
 class DrinkViewSet(viewsets.ModelViewSet):
     '''
